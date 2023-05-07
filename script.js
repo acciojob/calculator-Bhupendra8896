a3c8d71888856c9f4fd70cd7329837a4eddef650
bhupendra pratap <116386517+Bhupendra8896@users.noreply.github.com>
2023-05-07 15:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll(".button");
+let currentInput = "";
+let lastInput = "";
+let operator = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.innerText;
+
+    if (buttonText === "C") {
+      clear();
+    } else if (buttonText === "+" || buttonText === "-" || buttonText === "*" || buttonText === "/")
